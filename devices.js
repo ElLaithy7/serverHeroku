e1b6b855423e0f053e635ed9500e45485fe5b989
Meral Fahmi <meralfahmi@Merals-MacBook-Pro.local>
2021-05-26 12:01:43 +0200

    put request

--- devices.js
+++ devices.js
@@ -19,4 +19,57 @@ router.post("/", async(req, res) => {
     }
 
 })
+
+router.get('/:deviceID', (req, res) => {
+    const allDevices
+    try {
+        router.get("/", (req, res) => {
+            try {
+        
+                const devices = await device.find();
+                allDevices.send(devices)
+            } catch (err) {
+                allDevices.send("There was an error");
+            }
+           
+        })
+        const thisDevice = allDevices.find(c => c.deviceID === parseInt(req.params.deviceID));
+        if (!thisDevice) res.status(404).send('This device does not exist');
+        res.send(book);
+    }
+    catch (err) {
+        res.send({message : err})
+    }
+
+   
+    });
+
+router.put("/:deviceID", async (req, res) => {
+    const allDevices
+    try {
+        router.get("/", (req, res) => {
+            try {
+        
+                const devices = await device.find();
+                allDevices.send(devices)
+            } catch(err) {
+                allDevices.send("There was an error");
+        }
+        })
+        if (allDevices != "There was an error") {
+          
+                const thisDevice = allDevices.find(c => c.deviceID == toString(req.params.deviceID));
+            if (!thisDevice) res.status(404).send('Not Found');
+            
+            thisDevice.online = req.body.online;
+            thisDevice.hoster = req.body.hoster;
+            res.send(thisDevice);
+            
+            
+        }
+    }
+    catch (err) {
+        res.send({message: err})
+    }
+})
 module.exports = router
